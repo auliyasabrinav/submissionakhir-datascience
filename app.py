71e9ec87b6b88b5e397a8d79f45c06a8ba5d8df5
Auliya Sabrina <155187261+auliyasabrinav@users.noreply.github.com>
2025-05-15 15:42:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 import pandas as pd
 
 # Load model
-model = joblib.load('best_model.joblib')
+model = joblib.load('best_model.pkl')
 
 # Judul aplikasi
 st.title('Aplikasi Prediksi Status Mahasiswa')
